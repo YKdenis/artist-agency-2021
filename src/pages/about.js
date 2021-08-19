40a9e181b929fea3dfe41fc19bf21ca2914933f7
Denis <denis.inghelbrecht@outlook.be>
2021-08-19 17:30:20 +0200

    Finished React in Gatsby

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
-    <Layout pageTitle="About Me">
+    <Layout pageTitle="About Us">
       <p>Artist Agency was founded in 1977 by founder, John Doe. AA continues to be at the forefront of art by establishing the careers of our talents on a holistic level -- and setting trends within the industry. </p>
     </Layout>
   )
