d4e2980e9bf8918db56db625f1f27d46e4f576fd
Denis <denis.inghelbrecht@outlook.be>
2021-08-26 15:20:46 +0200

    programmatisch pagina's aanmaken finished

--- src/pages/artists/index.js
+++ src/pages/artists/index.js
@@ -1,13 +1,17 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
 
 const ArtistsPage = ({data: {allWpArtist: {edges}}}) => {
   return (
     <Layout pageTitle="Artists of Inghelbrecht Agency">
       {edges.map((item) => {
         const artist = item.node.artistMeta;
-        return <p>{artist.firstName} {artist.lastName}</p>
+        const slug = item.node.slug;
+        return <Link to={`/artists/${slug}`}>
+          <p key={item.node.id}>{artist.firstName} {artist.lastName}</p>
+        </Link>
+        
       })}
     </Layout>
   )
@@ -23,6 +27,8 @@ export const query = graphql`
           lastName
           artistName
         }
+				id
+        slug
       }
     }
   }
