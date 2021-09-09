8c919d1a9b2c49bbe2ec91dcad0650ef0d8077c6
Denis <denis.inghelbrecht@outlook.be>
2021-09-09 11:09:04 +0200

    Contact page: opmaak en styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,7 +41,6 @@ const Layout = ({ children }) => {
         <nav className={nav}>
           <header className={siteTitle}>{data.site.siteMetadata.title}</header>
           <ul className={navLinks}>
-            <li></li>
             <li className={navLinkItem}>
               <Link className={navLinkText} to="/">
                 Home
@@ -55,6 +54,11 @@ const Layout = ({ children }) => {
             <li className={navLinkItem}>
               <Link className={navLinkText} to="/artists">
                 Artists
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/contact">
+                Contact
               </Link>
             </li>
           </ul>
